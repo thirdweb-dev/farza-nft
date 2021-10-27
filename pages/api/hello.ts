bb98dc5a5944e862b6b80df414c82736a268e994
Ibrahim Ahmed <abeahmed2@gmail.com>
2021-10-26 18:15:37 -0700

    Use deep ai as a backup for generating style transfer

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,8 +1,29 @@
+import fs from "fs";
 import { NextApiRequest, NextApiResponse } from "next";
-import mutate from "../../lib/mutate";
+import path from "path";
+
+const DEEP_AI_API_KEY = process.env.DEEP_AI_API_KEY as string;
+
+const deepai = require("deepai");
+deepai.setApiKey(DEEP_AI_API_KEY);
 
 export default async function hello(req: NextApiRequest, res: NextApiResponse) {
-  await mutate();
+  console.log(deepai);
 
-  return res.status(200).json({});
+  const availableStyles = fs.readdirSync(path.resolve(".", "public/styles"));
+  const selectedStyle =
+    availableStyles[Math.floor(Math.random() * availableStyles.length)];
+  console.log("Selected style = ", selectedStyle);
+
+  var resp = await deepai.callStandardApi("fast-style-transfer", {
+    content: fs.createReadStream(path.resolve(".", "public/farza.jpeg")),
+    style: fs.createReadStream(
+      path.resolve(".", `public/styles/${selectedStyle}`)
+    ),
+  });
+  console.log(resp);
+
+  return res.status(200).json({
+    output_url: resp.output_url,
+  });
 }
--- pages/breed.tsx
+++ pages/breed.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@chakra-ui/button";
 import { Flex, Text, VStack } from "@chakra-ui/layout";
 import { ArbitraryStyleTransferNetwork } from "@magenta/image";
+import { useRouter } from "next/dist/client/router";
 import React, { useCallback, useEffect, useRef } from "react";
 
 export default function Breed() {
@@ -10,6 +11,8 @@ export default function Breed() {
   const styleRef = useRef<HTMLImageElement>();
 
   const outputImageRef = useRef<CanvasRenderingContext2D>();
+
+  const router = useRouter();
 
   useEffect(() => {
     if (model.current !== undefined) {
@@ -33,23 +36,35 @@ export default function Breed() {
 
     const result = await model.current.stylize(
       imageRef.current,
-      styleRef.current
+      styleRef.current,
+      0.5
     );
     console.log("result", result, outputImageRef.current);
     outputImageRef.current?.putImageData(result, 0, 0);
   }, [model, imageRef, styleRef, outputImageRef]);
+
+  const download = useCallback(() => {
+    outputImageRef.current?.canvas.toBlob((blob) => {
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "output.jpg";
+      a.click();
+      URL.revokeObjectURL(url);
+    });
+  }, [outputImageRef, router]);
 
   return (
     <>
       <Flex flexDir="row">
         <VStack mx={2}>
           <Text>Input</Text>
-          <img ref={imageRef} width="200" src="/farza.jpeg" />
+          <img ref={imageRef} width="500" src="/farza.jpeg" />
         </VStack>
 
         <VStack>
           <Text>Style</Text>
-          <img ref={styleRef} width="200" src="/style-1.png" />
+          <img ref={styleRef} width="500" src="/styles/jLgRcD0D0pI.jpg" />
         </VStack>
       </Flex>
       <Button my={2} mx={2} colorScheme="green" onClick={stylize}>
@@ -63,8 +78,9 @@ export default function Breed() {
           ) as CanvasRenderingContext2D;
         }}
         width="500"
-        height="500"
+        height="890"
       />
+      <Button onClick={download}>Download Image</Button>
     </>
   );
 }
